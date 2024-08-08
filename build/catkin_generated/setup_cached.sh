5d6c708d1a811c7818462928602c1e9e2d3579f6
shawbicc <faiyazsabik99@gmail.com>
2024-08-08 11:24:06 +0600

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/vboxuser/tutorial_ws/src/tutorial_pkg/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vboxuser/tutorial_ws/src/tutorial_pkg/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/vboxuser/tutorial_ws/src/tutorial_pkg/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/vboxuser/tutorial_ws/src/tutorial_pkg/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/vboxuser/tutorial_ws/src/tutorial_pkg/build'
+export ROS_PACKAGE_PATH="/home/vboxuser/tutorial_ws/src/tutorial_pkg:$ROS_PACKAGE_PATH"
